6eb7c5693f1fc911c83b19c2b5adcb48934d45c4
Hector Salgado <53808824+hector323@users.noreply.github.com>
2020-11-08 21:27:16 -0800

    updated

--- build.sh
+++ build.sh
@@ -1,37 +1,39 @@
 # Reads in the top.html
-# print('reading in html variables')
-top_html = open('./templates/top.html').read()
-# print(top_html)
+# # print('reading in html variables')
+# top_html = open('./templates/top.html').read()
+# # print(top_html)
 
-# Reads in the bottom.html
-bottom_html = open('./templates/bottom.html').read()
-# print(bottom_html)
+# # Reads in the bottom.html
+# bottom_html = open('./templates/bottom.html').read()
+# # print(bottom_html)
 
-# Reads in the middle index.html
-middle_html = open('./content/about.html').read()
-# print(middle_html)
+# # Reads in the middle index.html
+# middle_html = open('./content/about.html').read()
+# # print(middle_html)
 
-# combining all 3 files
-# Assembles hte new index.html file by combining the top middle and bottom in that order
-combined_html = top_html + middle_html + bottom_html
-# print(combined_html)
+# # combining all 3 files
+# # Assembles hte new index.html file by combining the top middle and bottom in that order
+# combined_html = top_html + middle_html + bottom_html
+# # print(combined_html)
 
-# Writes the new index.html file to a brand new file in the same directory
-open('./docs/index.html', 'w+').write(combined_html)
+# # Writes the new index.html file to a brand new file in the same directory
+# open('./docs/index.html', 'w+').write(combined_html)
 
 
-# Reads in the middle index.html
-middle_html = open('./content/projects.html').read()
+# # Reads in the middle index.html
+# middle_html = open('./content/projects.html').read()
 
-combined_html = top_html + middle_html + bottom_html
+# combined_html = top_html + middle_html + bottom_html
 
-# Writes the new index.html file to a brand new file in the same directory
-open('./docs/projects.html', 'w+').write(combined_html)
+# # Writes the new index.html file to a brand new file in the same directory
+# open('./docs/projects.html', 'w+').write(combined_html)
 
-# Reads in the middle index.html
-middle_html = open('./content/blog.html').read()
+# # Reads in the middle index.html
+# middle_html = open('./content/blog.html').read()
 
-combined_html = top_html + middle_html + bottom_html
+# combined_html = top_html + middle_html + bottom_html
 
-# Writes the new index.html file to a brand new file in the same directory
-open('./docs/blog.html', 'w+').write(combined_html)
+# # Writes the new index.html file to a brand new file in the same directory
+# open('./docs/blog.html', 'w+').write(combined_html)
+
+templates/top.html + content/index.html + templates/bottom.html = docs/index.html

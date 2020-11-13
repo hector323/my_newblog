817fed51bba1f9d481308eefdaa2e4921738e2ef
Hector Salgado <53808824+hector323@users.noreply.github.com>
2020-11-12 18:23:58 -0800

    updated bash file

---
+++ build.sh
@@ -0,0 +1,13 @@
+
+# cd templates
+cat templates/top.html ./content/about.html templates/bottom.html > docs/about.html
+
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
+
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
+
+
+# cat templates/top.html ./content/about.html templates/bottom.html > about.html
+
+
+cat docs/about.html docs/projects.html docs/blog.html > docs/index.html
